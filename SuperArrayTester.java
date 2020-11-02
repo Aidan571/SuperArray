82097cb3cf9c4687bb08bfb326190e8c028e52e3
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-11-02 14:45:11 -0500

    Tested add method.

--- SuperArrayTester.java
+++ SuperArrayTester.java
@@ -2,5 +2,6 @@ public class SuperArrayTester{
   public static void main(String[]args){
     SuperArray sa1 = new SuperArray();
     System.out.println(sa1.size());
+    System.out.println(sa1.add("Hello"));
   }
 }
