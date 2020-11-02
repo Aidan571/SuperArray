19960e5d199098c8b54e0baeda7cde4f57c317d7
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-11-02 14:38:18 -0500

    Created SuperArray class, instance variables, constructor, and size method.

---
+++ SuperArray.java
@@ -0,0 +1,14 @@
+public class SuperArray{
+  private String [] data;
+  private int size;
+
+  public SuperArray(){
+    size = 0;
+    data = new String [10];
+  }
+
+  public int size(){
+    return size;
+  }
+
+}
