e6308a2ce1b37e2fbc7660c271e8eadaa5cf7377
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-11-02 14:52:37 -0500

    Tested get method.

--- SuperArrayTester.java
+++ SuperArrayTester.java
@@ -3,5 +3,7 @@ public class SuperArrayTester{
     SuperArray sa1 = new SuperArray();
     System.out.println(sa1.size());
     System.out.println(sa1.add("Hello"));
+    System.out.println(sa1.get(0));
+    System.out.println(sa1.get(1));
   }
 }
