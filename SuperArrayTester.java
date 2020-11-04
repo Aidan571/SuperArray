6bb98f1696119e9f70172ca159708695130ec319
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-11-04 13:22:39 -0500

    tested indexOf method.

--- SuperArrayTester.java
+++ SuperArrayTester.java
@@ -37,5 +37,7 @@ public class SuperArrayTester{
     System.out.println(sa2.remove(2));
     System.out.println(sa2.toString());
     System.out.println(sa2.size());
+    System.out.println(sa2.indexOf("Hi"));
+    System.out.println(sa2.indexOf("Not Present"));
   }
 }
